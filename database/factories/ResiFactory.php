93e2b19e25d26bf62c9d130b0b2429b39985bd17
pimenvibritania <blackbrit13@gmail.com>
2019-06-29 14:46:41 +0700

    Rollack DB

---
+++ database/factories/ResiFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Resi;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Resi::class, function (Faker $faker) {
+    return [
+        'tglOrder' => $faker->date,
+        'invoice' => Str::random(10),
+        'nama' => $faker->name,
+        'noHp' => Str::random(12),
+        'produk'=> $faker->paragraph,
+        'provinsi'=>$faker->city,
+        'kota'=> $faker->city,
+        'kecamatan'=> $faker->city,
+        'alamat'=> $faker->city,
+        'resi'=>Str::random(10),
+
+
+    ];
+});
