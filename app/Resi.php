6d5e894023385a8fabf534168ceb03256ce084a3
pimenvibritania <blackbrit13@gmail.com>
2019-06-29 14:15:51 +0700

    Tinggal composer install, depedensi udah di update

---
+++ app/Resi.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Resi extends Model
+{
+    //
+    protected $fillable=[
+        'tglOrder', 'invoice', 'nama', 'noHp', 'produk',
+        'provinsi', 'kota', 'kecamatan','alamat','resi'
+
+    ];
+}
---
+++ database/migrations/2019_06_29_065803_create_resi_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resi', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('tglOrder');
+            $table->string('invoice');
+            $table->string('nama');
+            $table->string('noHp');
+            $table->string('produk');
+            $table->string('provinsi');
+            $table->string('kota');
+            $table->string('kecamatan');
+            $table->string('alamat');
+            $table->string('resi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resi');
+    }
+}
